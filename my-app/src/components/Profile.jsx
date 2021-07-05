bf2a159a7610f097991aabf1c228542347d80df9
kostitsyn <kostitsin.a@mail.ru>
2021-07-05 16:12:29 +0400

    12-е занятие

---
+++ my-app/src/components/Profile.jsx
@@ -0,0 +1,25 @@
+const Profile = () =>
+    <div className='content'>
+        <div>
+            <img src='https://cdn.mos.cms.futurecdn.net/wtqqnkYDYi2ifsWZVW2MT4-1200-80.jpg'></img>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    </div>
+
+export default Profile;
