08535297bf0ccba266a1897e0079a59a47f33254
kostitsyn <kostitsin.a@mail.ru>
2021-12-24 16:08:11 +0400

    Редактирование App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div>First block</div>
+      <div className="App">
+        <ul>
+          <li>React</li>
+          <li>JS</li>
+          <li>Django</li>
+          <li>Python</li>
+        </ul>
+      </div>
     </div>
   );
 }
