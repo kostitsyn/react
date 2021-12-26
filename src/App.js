f3f08d2f6b255da9edf57c79e5b517d690cb3a00
kostitsyn <kostitsin.a@mail.ru>
2021-12-26 15:16:59 +0400

    Добавил страницу Сообщения

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import Header from "./components/Header/Header";
 import UserContent from "./components/Profile/UserContent/UserContent";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import Messages from "./components/Messages/Messages";
 
 const App = () => {
     return (
         <div className='container'>
             <Header />
             <Navbar />
-            <Profile />
+            <div className='content'>
+                {/*<Messages />*/}
+                <Profile />
+            </div>
+
         </div>
     );
 }
