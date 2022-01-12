3742d13fa2656f532b9965eecafd1ab8529b74cc
kostitsyn <kostitsin.a@mail.ru>
2022-01-12 13:04:10 +0400

    Переписал Users на классы

--- django_server/django_server/settings.py
+++ django_server/django_server/settings.py
@@ -126,9 +126,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000'
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://127.0.0.1:3000'
+# ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
