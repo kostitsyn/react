50ea44dc30b2c20448ded432f862269e596c9d9a
kostitsyn <kostitsin.a@mail.ru>
2022-01-17 05:27:21 +0400

    Добавил модели профиля на сервере

--- django_server/mainapp/admin.py
+++ django_server/mainapp/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Profile, Photo, Contact
 
 admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(Photo)
+admin.site.register(Contact)
--- django_server/mainapp/models.py
+++ django_server/mainapp/models.py
@@ -12,3 +12,46 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Contact(models.Model):
+    facebook = models.URLField(max_length=256, verbose_name='Facebook')
+    website = models.URLField(max_length=256, verbose_name='Website')
+    twitter = models.URLField(max_length=256, verbose_name='Twitter')
+    instagram = models.URLField(max_length=256, verbose_name='Instagram')
+    youtube = models.URLField(max_length=256, verbose_name='Youtube')
+    github = models.URLField(max_length=256, verbose_name='Github')
+    main_link = models.URLField(max_length=256, verbose_name='Main link')
+
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'Контакты'
+
+
+class Photo(models.Model):
+    small = models.URLField(max_length=1024, verbose_name='Малое фото')
+    large = models.URLField(max_length=1024, verbose_name='Большое фото')
+
+    def __str__(self):
+        return f'Фотографии пользователя {self.__dict__}'
+
+    class Meta:
+        verbose_name = 'Фото'
+        verbose_name_plural = 'Фотографии'
+
+
+class Profile(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=256, verbose_name='Полное имя')
+    about_me = models.CharField(max_length=256, verbose_name='Обо мне')
+    looking_for_a_job = models.BooleanField(default=False, verbose_name='Ищу ли работу')
+    looking_for_a_job_description = models.CharField(max_length=512, verbose_name='Описание поиска работы')
+    photos = models.OneToOneField(Photo, null=True, blank=True, on_delete=models.CASCADE)
+    contacts = models.OneToOneField(Contact, null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Профиль пользователя {self.user_id.name}'
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
