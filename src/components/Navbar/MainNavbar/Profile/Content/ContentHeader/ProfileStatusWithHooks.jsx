88b7a6d8eb4d3831d2b6285ff4c061f1f65638cd
kostitsyn <kostitsin.a@mail.ru>
2022-03-07 14:44:46 +0400

    Добавил хук useEffect

--- src/components/Navbar/MainNavbar/Profile/Content/ContentHeader/ProfileStatusWithHooks.jsx
+++ src/components/Navbar/MainNavbar/Profile/Content/ContentHeader/ProfileStatusWithHooks.jsx
@@ -1,9 +1,14 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 
 const ProfileStatusWithHooks = props => {
     let [editMode, setEditMode] = useState(false);
     let [status, setStatus] = useState(props.status);
+
+    useEffect(() => {
+        debugger;
+        setStatus(props.status);
+    }, [props.status]);
 
     const activateEditMode = () => {
         setEditMode(true);
